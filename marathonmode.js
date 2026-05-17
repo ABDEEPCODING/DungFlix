e267268f25673044ff42dddf58e231fe060ba3cd
AB_DEEPCODING <24200@my.sanctamaria.school.nz>
2026-05-17 19:37:29 +1200

    Use YouTube embeds for marathon movies
    Replace Google Drive fileId entries with youtubeId properties and update player logic to use YouTube embed URLs. Added an actual youtubeId for "C.H.U.D." and left placeholder entries (youtubeId: null) for other titles with a note to replace them when real links are available. loadMarathonMovie now sets the iframe src to the YouTube embed or a temporary placeholder embed if no youtubeId is present.

--- marathonmode.js
+++ marathonmode.js
@@ -1,15 +1,17 @@
 let currentIndex = 0;
 
 const marathonMovies = [
-    { title: "C.H.U.D.", fileId: "1DVucLoYKHbT3FHqG4BwRx9A0c95zoSLL", thumbnail: "Movie_thumbnails/chud.png" },
-    { title: "The Reconciler", fileId: "1bX0oHHq470ZUpcZC4K9HZEpsc8Vkxl8q", thumbnail: "Movie_thumbnails/reconciler.png" },
-    { title: "The Amazing Bulk", fileId: "1AScuz9A8GWDqKR1jDiIP7Z4Jp6orowUD", thumbnail: "Movie_thumbnails/amazing_bulk.png" },
-    { title: "Attack of the Killer Tomatoes", fileId: "1jWofaMO6ifM-IPEqJ5HtWFrgmTf1veYS", thumbnail: "Movie_thumbnails/Attack_of_the_Killer_Tomatoes.png" },
-    { title: "Piranha", fileId: "19huyMBe-u7dgQYrdQdzKmcWB8qH6pTPp", thumbnail: "Movie_thumbnails/Piranha.png" },
-    { title: "Birdemic: Shock and Terror", fileId: "1PhnWw2Ac9lz2stTS6VW41DIu3e-_mn2G", thumbnail: "Movie_thumbnails/Birdemic.png" },
-    { title: "C.H.U.D. II", fileId: "1t31WVvTZXQ2lUUo4_5Ciw5wDSDAfNv0j", thumbnail: "Movie_thumbnails/Chud_two.png" },
-    { title: "Titanic II", fileId: "1eYgqSjBgKfKYG7-UoMxls8Vr-MAyICko", thumbnail: "Movie_thumbnails/titanic_two.png" },
-    { title: "Super Mario Bros (Morton Jankel Cut)", fileId: "1dNQqMjBO-eoONfGnv20vUCEisbaHPHWh", thumbnail: "Movie_thumbnails/Super_Mario_Bros.png" }
+    { title: "C.H.U.D.", youtubeId: "lTvfzolWRBw", thumbnail: "Movie_thumbnails/chud.png" },
+
+
+    { title: "The Reconciler", youtubeId: null, thumbnail: "Movie_thumbnails/reconciler.png" },
+    { title: "The Amazing Bulk", youtubeId: null, thumbnail: "Movie_thumbnails/amazing_bulk.png" },
+    { title: "Attack of the Killer Tomatoes", youtubeId: null, thumbnail: "Movie_thumbnails/Attack_of_the_Killer_Tomatoes.png" },
+    { title: "Piranha", youtubeId: null, thumbnail: "Movie_thumbnails/Piranha.png" },
+    { title: "Birdemic: Shock and Terror", youtubeId: null, thumbnail: "Movie_thumbnails/Birdemic.png" },
+    { title: "C.H.U.D. II", youtubeId: null, thumbnail: "Movie_thumbnails/Chud_two.png" },
+    { title: "Titanic II", youtubeId: null, thumbnail: "Movie_thumbnails/titanic_two.png" },
+    { title: "Super Mario Bros (Morton Jankel Cut)", youtubeId: null, thumbnail: "Movie_thumbnails/Super_Mario_Bros.png" }
 ];
 
 function loadMarathonMovie() {
@@ -18,11 +20,13 @@ function loadMarathonMovie() {
     document.getElementById("now-watching-img").src = movie.thumbnail;
     document.getElementById("movie-title").textContent = movie.title;
 
-    if (movie.fileId) {
+    if (movie.youtubeId) {
         document.getElementById("movie-frame").src =
-            `https://drive.google.com/file/d/${movie.fileId}/preview`;
+            `https://www.youtube.com/embed/${movie.youtubeId}`;
     } else {
-        document.getElementById("movie-frame").src = "";
+        // Placeholder video (YouTube blank screen)
+        document.getElementById("movie-frame").src =
+            "https://www.youtube.com/embed/dQw4w9WgXcQ"; 
     }
 
     document.getElementById("progress-text").textContent =
